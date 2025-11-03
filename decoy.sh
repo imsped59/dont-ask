03bb3bd482172bcc24dd65ea35acd75fc056f2a9
imsped59 <bobisalivehaha@proton.me>
2025-11-03 18:31:26 -0500

    Create decoy.sh

---
+++ decoy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# decoy.sh - Start decoy + botter
+echo "👻 Microwave Blooket Botter Active 👻"
+echo "Stealth: Tor-proxied, appliance UA"
+echo "Decoy traffic: Running"
+proxychains python /botter.py
